8d50830a018a57452a8a0cbaac8d1345a417f5e2
hentmade <maxhentschel@gmx.de>
2023-11-08 11:13:13 +0100

    added slideshow

--- script/navbar.js
+++ script/navbar.js
@@ -5,15 +5,15 @@ function toggleMenu() {
 
       switch(activeNavHeight)
       {
-         case 50:    
-            document.getElementById("inside_nav").style.height = "350px";
+         case 30:    
+            document.getElementById("inside_nav").style.height = "330px";
             document.getElementById("nav_ul").style.display = "flex";
             document.getElementById("nav_ul").style.height = "300px";
             document.getElementById("menubutton").src = "./img/nav/menuclose.svg";
             break;
    
-         case 350:   
-            document.getElementById("inside_nav").style.height = "50px";
+         case 330:   
+            document.getElementById("inside_nav").style.height = "30px";
             document.getElementById("nav_ul").style.display = "none";
             document.getElementById("menubutton").src = "./img/nav/menuburger.svg";
             break;
